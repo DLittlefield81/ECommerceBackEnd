0b48942bd306c7c466544d288f996103394e4bde
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-06-23 20:10:08 -0400

    setup models, need to finish product

---
+++ models/index.js
@@ -0,0 +1,32 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+// Categories have many Products
+Category.hasMany(Product);
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
